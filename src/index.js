792f2c2cfe1572a6265afe65205554f48047b802
Alexander Tikhonov <at1385@yandex.ru>
2022-02-24 01:17:27 +0500

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2) return false;
+
+  const openedBrackets = [];
+  bracketsConfig.forEach((item) => openedBrackets.push(item[0]));
+
+  const openedBracketToClosed = Object.fromEntries(bracketsConfig);
+
+  const stack = [];
+
+  for (let char of str) {
+    if ((openedBracketToClosed[stack[stack.length - 1]]) === char) {
+      stack.pop();
+    } else if (openedBrackets.includes(char)) {
+      stack.push(char);
+    }
+  }
+
+  return !stack.length;
 }
